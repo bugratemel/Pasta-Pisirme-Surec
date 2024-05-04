1dde393b6c80b47090c052658e8e3091775eff49
bugratemel <151394448+bugratemel@users.noreply.github.com>
2024-05-04 14:06:48 +0300

    Add files via upload

---
+++ ProgressBarTimer/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace ProgressBarTimer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.progressBar4 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(0, 59);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(729, 37);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Bisque;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(11, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(391, 37);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Un ve yumurta karıştırılıyor...";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Bisque;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(11, 159);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(333, 37);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Çırpma işlemi yapılıyor...";
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(0, 210);
+            this.progressBar2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(729, 37);
+            this.progressBar2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Bisque;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(11, 314);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(516, 37);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Malzemeler ekleniyor ve karıştırılıyor...";
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.Location = new System.Drawing.Point(0, 364);
+            this.progressBar3.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.Size = new System.Drawing.Size(729, 37);
+            this.progressBar3.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Bisque;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(11, 478);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(235, 37);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Pasta pişiriliyor...";
+            // 
+            // progressBar4
+            // 
+            this.progressBar4.Location = new System.Drawing.Point(0, 530);
+            this.progressBar4.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.progressBar4.Name = "progressBar4";
+            this.progressBar4.Size = new System.Drawing.Size(729, 37);
+            this.progressBar4.TabIndex = 6;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 3000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 1000;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Interval = 6000;
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 27F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(728, 616);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.progressBar4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progressBar3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Font = new System.Drawing.Font("MS PGothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar progressBar3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ProgressBar progressBar4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+    }
+}
+
---
+++ ProgressBarTimer/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgressBarTimer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        DialogResult sonuc;
+        DialogResult sonuc2;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            progressBar1.Value += 10;
+            if (progressBar1.Value % 20 == 10)
+            {
+                label1.BackColor = Color.Red;
+            }
+            if (progressBar1.Value % 20 == 0)
+            {
+                label1.BackColor = Color.Yellow;
+            }
+            if (progressBar1.Value == 100)
+            {
+                timer1.Stop();
+                sonuc = MessageBox.Show("Karıştırma İşlemi bitti sıradaki işleme geçmek istiyorsanız evet butonuna tıklayınız", "BİLGİLENDİRME", MessageBoxButtons.YesNo, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1);
+                if (sonuc == DialogResult.Yes)
+                {
+                    timer2.Start();
+                }
+                else
+                {
+                    sonuc2 = MessageBox.Show("Uygulamayı yeniden başlatmak istediğinizde burdaki buton'da tamama tıklayınız", "UYARI", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    if (sonuc2 == DialogResult.OK)
+                    {
+                        timer2.Start();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Uygulamadan Çıkılıyor...", "HATA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        Application.Exit();
+                    }
+                }
+             
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            progressBar2.Value += 10;
+            if (progressBar2.Value % 20 == 10)
+            {
+                label2.BackColor = Color.Blue;
+            }
+            if (progressBar2.Value % 20 == 0)
+            {
+                label2.BackColor = Color.Purple;
+            }
+            if (progressBar2.Value == 100)
+            {
+                timer2.Stop();
+                sonuc = MessageBox.Show("Çırpma İşlemi bitti sıradaki işleme geçmek istiyorsanız evet butonuna tıklayınız", "BİLGİLENDİRME", MessageBoxButtons.YesNo, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1);
+                if (sonuc == DialogResult.Yes)
+                {
+                    timer3.Start();
+                }
+                else
+                {
+                    sonuc2 = MessageBox.Show("Uygulamayı yeniden başlatmak istediğinizde burdaki buton'da tamama tıklayınız", "UYARI", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    if (sonuc2 == DialogResult.OK)
+                    {
+                        timer3.Start();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Uygulamadan Çıkılıyor...", "HATA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        Application.Exit();
+                    }
+                }
+            }
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            progressBar3.Value += 10;
+            if (progressBar3.Value % 20 == 10)
+            {
+                label3.BackColor = Color.White;
+            }
+            if (progressBar3.Value % 20 == 0)
+            {
+                label3.BackColor = Color.Black;
+            }
+            if (progressBar3.Value == 100)
+            {
+                timer3.Stop();
+                sonuc = MessageBox.Show("Malzemeleri ekleme ve karıştırma İşlemi bitti sıradaki işleme geçmek istiyorsanız evet butonuna tıklayınız", "BİLGİLENDİRME", MessageBoxButtons.YesNo, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1);
+                if (sonuc == DialogResult.Yes)
+                {
+                    timer4.Start();
+                }
+                else
+                {
+                    sonuc2 = MessageBox.Show("Uygulamayı yeniden başlatmak istediğinizde burdaki buton'da tamama tıklayınız", "UYARI", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    if (sonuc2 == DialogResult.OK)
+                    {
+                        timer4.Start();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Uygulamadan Çıkılıyor...", "HATA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        Application.Exit();
+                    }
+                }
+            }
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            progressBar4.Value += 1;
+            if (progressBar4.Value % 2 == 1)
+            {
+                label4.BackColor = Color.Orange;
+            }
+            if (progressBar4.Value % 2 == 0)
+            {
+                label4.BackColor = Color.Brown;
+            }
+            if (progressBar4.Value == 100)
+            {
+                timer4.Stop();
+                MessageBox.Show("Pasta pişirme işlemi bitti :)", "BİLGİLENDİRME", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1);
+            }
+        }
+    }
+}
